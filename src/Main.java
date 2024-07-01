5e24ece91ec06184d07d6db774b57f4c7a1b818f
ernaks <eren3246@gmail.com>
2024-07-01 18:04:03 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int year;
+        System.out.print("Yıl giriniz: ");
+        year = sc.nextInt();
+        if((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0)){
+            System.out.println(year + " bir artık yıldır.");
+        }
+        else
+            System.out.println(year + " bir artık yıl değildir.");
+    }
+}
